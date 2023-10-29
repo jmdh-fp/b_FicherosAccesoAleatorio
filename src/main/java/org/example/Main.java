f1236db2a08bcc3c63d1c067bf6152c507a799e9
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2023-10-29 11:20:25 +0100

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-         //EjemplosVarios.escribirEntero();
+         EjemplosVarios.escribirEntero();
         //EjemplosVarios.modificarEntero();
         //EjemplosVarios.mostrarFichero();
-        EjemplosVarios.palabras();
-
     }
 }
